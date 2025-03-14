30af5ed49ac32b8bd85b3341cd13fd9e1e16fa96
ye21iin <kathykr120@gmail.com>
2025-03-14 11:52:53 +0900

    2단계 완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,33 @@
-export default function Home() {
-  return <div>Home</div>;
+type Intro = {
+  description1: string;
+  description2: string;
+  description3: string;
+};
+
+export default async function Home() {
+  const intro: Intro = await fetch("http://localhost:4002/intro", {
+    cache: "force-cache",
+  }).then((response) => response.json());
+
+  if (!intro) return;
+
+  return (
+    <div>
+      <div>
+        <h1>React to Next.js 마이그레이션 프로젝트</h1>
+        <div>
+          <p>
+            SSG 페이지는 빌드 시점에 데이터를 미리 가져와서 캐싱 후, 사용자 요청
+            시 캐싱해둔 데이터로 페이지를 렌더링하는 방식입니다.
+          </p>
+          <p>변경되지 않는 정적인 페이지에 적절한 렌더링 방식입니다.</p>
+        </div>
+        <div className="flex flex-col gap-10">
+          <p>{intro.description1}</p>
+          <p>{intro.description2}</p>
+          <p>{intro.description3}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
