660411b4b9a511a4a7313b8d0171efe850c16113
ye21iin <kathykr120@gmail.com>
2025-03-14 12:55:29 +0900

    5단계 미완료

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import Link from "next/link";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import Provider from "./components/Provider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,9 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <nav>
           <ul className="flex justify-evenly">
             <li>
@@ -46,7 +33,7 @@ export default function RootLayout({
             </li>
           </ul>
         </nav>
-        {children}
+        <Provider>{children}</Provider>
       </body>
     </html>
   );
--- src/app/todolist/page.tsx
+++ src/app/todolist/page.tsx
@@ -1,3 +1,24 @@
+import TodoForm from "../components/TodoForm";
+import TodoItem from "../components/TodoItem";
+
 export default function TodoList() {
-  return <div>TodoList</div>;
+  return (
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <h2 className="blog-main-title">서버통신 투두리스트 by useState</h2>
+      <div className="description-text">
+        <p>
+          Next.js에서 CSR은 마운트 시점까지의 데이터는 미리 프리렌더링해두고,
+          사용자 요청에 따라 데이터를 가져와서 페이지를 렌더링하는 방식입니다.
+        </p>
+        <p>
+          CSR 페이지는 사용자와의 인터랙션이 있는 페이지에 적절한 렌더링
+          방식입니다.
+        </p>
+      </div>
+      <TodoForm />
+      <TodoItem />
+    </div>
+  );
 }
