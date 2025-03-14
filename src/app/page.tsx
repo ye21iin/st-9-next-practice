404dbf0773842a13414b0af33f8ddc2118177bc5
ye21iin <kathykr120@gmail.com>
2025-03-14 11:55:56 +0900

    2단계css 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,15 +14,19 @@ export default async function Home() {
   return (
     <div>
       <div>
-        <h1>React to Next.js 마이그레이션 프로젝트</h1>
+        <h1 className="font-extrabold text-4xl mt-5">
+          React to Next.js 마이그레이션 프로젝트
+        </h1>
         <div>
-          <p>
+          <p className="text-red-500 m-5">
             SSG 페이지는 빌드 시점에 데이터를 미리 가져와서 캐싱 후, 사용자 요청
             시 캐싱해둔 데이터로 페이지를 렌더링하는 방식입니다.
           </p>
-          <p>변경되지 않는 정적인 페이지에 적절한 렌더링 방식입니다.</p>
+          <p className="text-gray-500 m-5">
+            변경되지 않는 정적인 페이지에 적절한 렌더링 방식입니다.
+          </p>
         </div>
-        <div className="flex flex-col gap-10">
+        <div className="flex flex-col gap-10 m-5">
           <p>{intro.description1}</p>
           <p>{intro.description2}</p>
           <p>{intro.description3}</p>
