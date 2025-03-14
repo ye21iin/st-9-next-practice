971fdb959026550329a3c93de81520509b333690
ye21iin <kathykr120@gmail.com>
2025-03-14 16:11:22 +0900

    lazy initialization

--- src/app/components/Provider.tsx
+++ src/app/components/Provider.tsx
@@ -1,33 +1,9 @@
 "use client";
-import {
-  isServer,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
-
-function makeQueryClient() {
-  return new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: 60 * 1000,
-      },
-    },
-  });
-}
-
-let browserQueryClient: QueryClient | undefined = undefined;
-
-function getQueryClient() {
-  if (isServer) {
-    return makeQueryClient();
-  } else {
-    if (!browserQueryClient) browserQueryClient = makeQueryClient();
-    return browserQueryClient;
-  }
-}
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
-  const queryClient = getQueryClient();
+  const [queryClient] = useState<QueryClient>(() => new QueryClient()); // lazy-initialization
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
